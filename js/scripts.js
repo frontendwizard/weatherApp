7c1d72f0f88b8094a73e05ee263aea35c9d71515
Juliano Rafael <julianorsfarias@msn.com>
2015-09-02 09:54:58 -0300

    Cleaner js

--- js/scripts.js
+++ js/scripts.js
@@ -132,22 +132,6 @@ $(document).ready(function() {
           // NEXT HOURS WEATHER DATA
           hours_summary = forecastDataParse.hourly.summary;
           hours_icon = forecastDataParse.hourly.icon;
-          // for (i=0; i<forecastDataParse.hourly.data; i++) {
-          //   hour_time[i] = forecastDataParse.hourly.data[i].time;
-          //   hour_summary[i] = forecastDataParse.hourly.data[i].summary;
-          //   hour_icon[i] = forecastDataParse.hourly.data[i].icon;
-          //   hour_precip_intensity[i] = forecastDataParse.hourly.data[i].precipIntesity;
-          //   hour_precip_probability[i] = forecastDataParse.hourly.data[i].precipProbability;
-          //   hour_temp[i] = forecastDataParse.hourly.data[i].temperature;
-          //   hour_apparent_temp[i] = forecastDataParse.hourly.data[i].apparentTemperature;
-          //   hour_dew_point[i] = forecastDataParse.hourly.data[i].dewPoint;
-          //   hour_humidity[i] = forecastDataParse.hourly.data[i].humidity;
-          //   hour_wind_speed[i] = forecastDataParse.hourly.data[i].windSpeed;
-          //   hour_wind_bearing[i] = forecastDataParse.hourly.data[i].windBearing;
-          //   hour_cloud_cover[i] = forecastDataParse.hourly.data[i].cloudCover;
-          //   hour_pressure[i] = forecastDataParse.hourly.data[i].pressure;
-          //   hour_ozone[i] = forecastDataParse.hourly.data[i].ozone;
-          // }
 
           $('#hours_summary').html(hours_summary);
           $('#hours_icon').addClass('wi-forecast-io-'+hours_icon);
@@ -157,35 +141,6 @@ $(document).ready(function() {
           days_icon = forecastDataParse.daily.icon;
           day_temp_max = Math.round(forecastDataParse.daily.data[0].temperatureMax);
           day_temp_min = Math.round(forecastDataParse.daily.data[0].temperatureMin);
-
-          // for (i=0; i<forecastDataParse.hourly.data; i++) {
-          //   day_time[i] = forecastDataParse.daily.data[i].time;
-          //   day_summary[i] = forecastDataParse.daily.data[i].summary;
-          //   day_icon[i] = forecastDataParse.daily.data[i].icon;
-          //   day_sunrise_time[i] = forecastDataParse.daily.data[i].sunriseTime;
-          //   day_sunset_time[i] = forecastDataParse.daily.data[i].sunsetTime;
-          //   day_moon_phase[i] = forecastDataParse.daily.data[i].moonPhase;
-          // //   day_precip_intensity[i] = forecastDataParse.daily.data[i].precipIntesity;
-          // //   day_precip_intensity_max[i] = forecastDataParse.daily.data[i].precipIntesityMax;
-          // //   day_precip_intensity_max_time[i] = forecastDataParse.daily.data[i].precipIntesityMaxTime;
-          // //   day_precip_probability[i] = forecastDataParse.daily.data[i].precipProbability;
-          // //   day_precip_type[i] = forecastDataParse.daily.data[i].precipType;
-          //   day_temp_min[i] = forecastDataParse.daily.data[i].temperatureMin;
-          // //   day_temp_min_time[i] = forecastDataParse.daily.data[i].temperatureMinTime;
-          //   day_temp_max[i] = forecastDataParse.daily.data[i].temperatureMax;
-          // //   day_temp_max_time[i] = forecastDataParse.daily.data[i].temperatureMaxTime;
-          // //   day_apparent_temp_min[i] = forecastDataParse.daily.data[i].apparentTemperatureMin;
-          // //   day_apparent_temp_min_time[i] = forecastDataParse.daily.data[i].apparentTemperatureMinTime;
-          // //   day_apparent_temp_max[i] = forecastDataParse.daily.data[i].apparentTemperatureMax;
-          // //   day_apparent_temp_max_time[i] = forecastDataParse.daily.data[i].apparentTemperatureMaxTime;
-          // //   day_dew_point[i] = forecastDataParse.daily.data[i].dewPoint;
-          // //   day_humidity[i] = forecastDataParse.daily.data[i].humidity;
-          // //   day_wind_speed[i] = forecastDataParse.daily.data[i].windSpeed;
-          // //   day_wind_bearing[i] = forecastDataParse.daily.data[i].windBearing;
-          // //   day_cloud_cover[i] = forecastDataParse.daily.data[i].cloudCover;
-          // //   day_pressure[i] = forecastDataParse.daily.data[i].pressure;
-          // //   day_ozone[i] = forecastDataParse.daily.data[i].ozone;
-          // }
 
           $('#days_summary').html(days_summary);
           $('#days_icon').addClass('wi-forecast-io-'+days_icon);
